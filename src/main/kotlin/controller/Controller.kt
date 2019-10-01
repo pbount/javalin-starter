9a3eb7bc2c8059fb7e9db9e5ed21f0dbc3f4350b
pbount <GQqt9TWypJ4NSZCLx3FW>
2019-10-01 19:53:23 +0100

    Added comments

--- src/main/kotlin/controller/Controller.kt
+++ src/main/kotlin/controller/Controller.kt
@@ -3,7 +3,20 @@ package controller
 import io.javalin.apibuilder.ApiBuilder.*
 
 class Controller {
+
     fun registerRoutes(){
+
+        /**
+         * Example simple get route
+         */
+        App.javalin.get("/") {
+                ctx -> ctx.result("${ctx.status()}")
+        }
+
+        /**
+         * Example get route with path parameter
+         * Visiting 'host:port/test/property will print 'property'
+         */
         App.javalin.routes {
             path("v1"){
                 get("test/:property") {
@@ -11,7 +24,5 @@ class Controller {
                 }
             }
         }
-
-        App.javalin.get("/") { ctx -> ctx.result("${ctx.status()}") }
     }
 }
