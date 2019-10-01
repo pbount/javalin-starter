92c618f13cbeb3d6c1d1513274b2bf963816f339
pbount <GQqt9TWypJ4NSZCLx3FW>
2019-10-01 19:57:00 +0100

    Added comments

--- src/main/kotlin/Run.kt
+++ src/main/kotlin/Run.kt
@@ -6,6 +6,12 @@ object App {
     val javalin: Javalin = Javalin.create()
 }
 
+/**
+ * Execute this function to start the app
+ * Run gradle build task 'fatJar' to create a complete, self-contained jar file.
+ * Execute the jar file using:
+ * # java -jar <jar-path>
+ */
 fun main() {
     javalin.start()
     AppModule.GuiceInjectorProvider.injector.getInstance(Controller::class.java).registerRoutes()
